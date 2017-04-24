7359c4286a1762bb5cd1f2d10b2ee4603e685c30
Tospaa <musaecer@gmail.com>
2017-04-25 01:39:33 +0300

    Update changeListener.py
    Added missing process checking function. Now the script requires psutil module.

--- changeListener.py
+++ changeListener.py
@@ -1,9 +1,11 @@
 import os
 import time
 import shutil
+import psutil
 
 FILE_TO_WATCH = r"C:\Users\YOUR-USER-NAME\Documents\My Games\SOME-GAME\saves\save.sav"  # The save file to monitor changes.
 BACKUP_DIRECTORY = r"C:\Users\YOUR-USER-NAME\Documents\My Games\SOME-GAME\saves\Backups"  # Backup directory to backup save files.
+PROCESS_IMAGE_NAME = "explorer.exe"  # The name of the process.
 
 
 def find_ext(file_path):
@@ -23,9 +25,10 @@ def find_filename(file_path):
     raise ValueError("Internal error. That shouldn't have happened.")
 
 class changeListener(object):
-    def __init__(self, filetw, backupdir):
+    def __init__(self, filetw, backupdir, process_im):
         self.file = filetw
         self.backupdir = backupdir
+        self.process_im = process_im
         self.backup_generic = r"\backup{0}" + find_ext(self.file)
         self.cached_stamp = os.stat(self.file).st_mtime
         self.file_list = []
@@ -34,12 +37,12 @@ class changeListener(object):
             os.makedirs(self.backupdir)
         with os.scandir(self.backupdir) as it:
             for entry in it:
-                if not entry.name.startswith('.') and entry.is_file():
+                if entry.name.startswith('backup') and entry.is_file():
                     self.file_list.append(entry.name)
                     self.file_count+=1
 
     def check_process(self):
-        return True
+        return process_im in (p.name() for p in psutil.process_iter())
 
     def listen(self):
         time.sleep(30)
@@ -55,7 +58,7 @@ class changeListener(object):
 
 
 if __name__ == "__main__":
-    listener = changeListener(FILE_TO_WATCH, BACKUP_DIRECTORY)
+    listener = changeListener(FILE_TO_WATCH, BACKUP_DIRECTORY, PROCESS_IMAGE_NAME)
     print("Listener started successfully.")
     # time.sleep(5)
     while listener.check_process():
