01eb8aa7b388c07ea8e3bd06575ed507b704c489
Tospaa <musaecer@gmail.com>
2017-04-24 20:51:56 +0300

    Update changeListener.py
    Script no longer (hopefully) overwrites other backups.

--- changeListener.py
+++ changeListener.py
@@ -4,23 +4,39 @@ import shutil
 
 FILE_TO_WATCH = r"C:\Users\YOUR-USER-NAME\Documents\My Games\SOME-GAME\saves\save.sav"  # The save file to monitor changes.
 BACKUP_DIRECTORY = r"C:\Users\YOUR-USER-NAME\Documents\My Games\SOME-GAME\saves\Backups"  # Backup directory to backup save files.
-BACKUP_GENERIC_NAME = r"\backup{0}.sav"  """ File format extension of the generic name should
-                                             be equal with the extension of the save file.
-                                             DO NOT ERASE OR EDIT {0} PART OF THE GENERIC NAME
-                                             IF YOU DON'T KNOW WHAT YOU ARE DOING!!!"""
+
+
+def find_ext(file_path):
+    indx = -1
+    for _ in range(len(file_path)):
+        if file_path[indx] == ".":
+            return file_path[indx:]
+        indx-=1
+    return ""
+
+def find_filename(file_path):
+    indx = -1
+    for _ in range(len(file_path)):
+        if file_path[indx] == "\\":
+            return file_path[indx+1:]
+        indx-=1
+    raise ValueError("Internal error. That shouldn't have happened.")
 
 class changeListener(object):
-    def __init__(self, filetw, backupdir, backup_generic):
+    def __init__(self, filetw, backupdir):
         self.file = filetw
         self.backupdir = backupdir
-        self.backup_generic = backup_generic
+        self.backup_generic = r"\backup{0}" + find_ext(self.file)
         self.cached_stamp = os.stat(self.file).st_mtime
+        self.file_list = []
         self.file_count = 0
+        if not os.path.exists(self.backupdir):
+            os.makedirs(self.backupdir)
         with os.scandir(self.backupdir) as it:
             for entry in it:
                 if not entry.name.startswith('.') and entry.is_file():
-                    self.file_count+=1  """ This piece of code assumes that you only have
-                                            backups in your backup directory."""
+                    self.file_list.append(entry.name)
+                    self.file_count+=1
 
     def check_process(self):
         return True
@@ -31,12 +47,15 @@ class changeListener(object):
             self.cached_stamp = os.stat(self.file).st_mtime
             self.file_count+=1
             filename = self.backupdir + self.backup_generic.format(self.file_count)
+            while find_filename(filename) in self.file_list:
+                self.file_count+=1
+                filename = self.backupdir + self.backup_generic.format(self.file_count)
             shutil.copyfile(self.file, filename)
             print("Backup copied successfully.")
 
 
 if __name__ == "__main__":
-    listener = changeListener(FILE_TO_WATCH, BACKUP_DIRECTORY, BACKUP_GENERIC_NAME)
+    listener = changeListener(FILE_TO_WATCH, BACKUP_DIRECTORY)
     print("Listener started successfully.")
     # time.sleep(5)
     while listener.check_process():
