08434da4b6c04fa856a0a1f6c006a19438b6c9d0
Tospaa <musaecer@gmail.com>
2020-02-21 17:14:49 +0300

    added timestamp and file path to log
    also, i have introduced new generic backup name, but have not removed old format due to backward compatibility. flip LEGACY variable to False to see the changes.

--- changeListener.py
+++ changeListener.py
@@ -1,5 +1,6 @@
 import os
 import time
+from datetime import datetime
 import shutil
 try:
     import psutil
@@ -15,6 +16,9 @@ except:
 FILE_TO_WATCH = r"C:\Users\YOUR-USER-NAME\Documents\My Games\SOME-GAME\saves\save.sav"  # The save file to monitor changes. Use FULL PATH.
 BACKUP_DIRECTORY = r"C:\Users\YOUR-USER-NAME\Documents\My Games\SOME-GAME\saves\Backups"  # Backup directory to backup save files. Use FULL PATH.
 PROCESS_IMAGE_NAME = "explorer.exe"  # The name of the process.
+LEGACY = True  # You can flip this to False (actually, it is recommended) to try the new generic backup name.
+               # New generic backup name? What? Surprise, surprise! It is based on time!
+               # I mean, you already made me import datetime. Why not use it here as well?
 
 
 class changeListener(object):
@@ -22,17 +26,19 @@ class changeListener(object):
         self.file = filetw
         self.backupdir = backupdir
         self.process_im = process_im
-        self.backup_generic = r"\backup{0}-" + self.find_filename(self.file)
         self.cached_stamp = os.stat(self.file).st_mtime
-        self.file_list = []
-        self.file_count = 0
         if not os.path.exists(self.backupdir):
             os.makedirs(self.backupdir)
-        with os.scandir(self.backupdir) as it:
-            for entry in it:
-                if entry.name.startswith('backup') and entry.is_file():
-                    self.file_list.append(entry.name)
-                    self.file_count+=1
+        if LEGACY:
+            self.backup_generic = "backup{0}-" + self.find_filename(self.file)  # This is legacy.
+            self.file_list = []  # This is legacy.
+            self.file_count = 0  # This is also legacy.
+            with os.scandir(self.backupdir) as it:
+                # This code block will likely be removed in the next version.
+                for entry in it:
+                    if entry.name.startswith('backup') and entry.is_file():
+                        self.file_list.append(entry.name)
+                        self.file_count+=1
         if GNTP:
             self.notifier = gntp.notifier.GrowlNotifier(
                 applicationName = "Change Listener",
@@ -48,6 +54,10 @@ class changeListener(object):
                 self.notifier.register()
 
     def notify(self, message, stcky = False, prio = 0):
+        message = "[{timestamp}] {message}".format(
+            timestamp=datetime.now().strftime("%Y/%m/%d %H:%M:%S"),
+            message=message
+        )
         if GNTP:
             self.notifier.notify(
                 noteType = "Message",
@@ -59,18 +69,16 @@ class changeListener(object):
             print(message)
 
     def find_filename(self, file_path):
-        indx = -1
-        for _ in file_path:
-            if file_path[indx] == "\\":
-                return file_path[indx+1:]
-            indx-=1
-        self.notify("Faced an error while handling filename.", True, 2)
-        # If you don't use full path, this error is going to issue.
-        if GNTP:
-            raise SystemExit
-        else:
-            input("Press Enter to exit.")
-            raise SystemExit
+        try:
+            return os.path.basename(file_path)
+        except:
+            self.notify("Faced an error while handling filename.", True, 2)
+            # If you don't use full path, this error is going to issue.
+            if GNTP:
+                raise SystemExit
+            else:
+                input("Press Enter to exit.")
+                raise SystemExit
 
     def check_process(self):
         if PSUTIL:
@@ -88,13 +96,19 @@ class changeListener(object):
                 but write it part by part. This situation ends up with 5 or 6 backup files but
                 just one of them meant to be. By adding this while loop with 15 seconds interval,
                 I'm aiming to bypass the mentioned situation."""
-            self.file_count+=1
-            filename = self.backupdir + self.backup_generic.format(self.file_count)
-            while self.find_filename(filename) in self.file_list:
+            if LEGACY:
                 self.file_count+=1
-                filename = self.backupdir + self.backup_generic.format(self.file_count)
+                filename = os.path.join(self.backupdir, self.backup_generic.format(self.file_count))
+                while self.find_filename(filename) in self.file_list:
+                    self.file_count+=1
+                    filename = os.path.join(self.backupdir, self.backup_generic.format(self.file_count))
+            else:
+                filename = os.path.join(self.backupdir, "bkp-{time}-{name}".format(
+                    time=datetime.now().strftime('%Y%m%d%H%M%S'),
+                    name=self.find_filename(self.file)
+                ))
             shutil.copyfile(self.file, filename)
-            self.notify("Backup copied successfully.")
+            self.notify("Backup copied successfully. File path: {}".format(filename))
 
 
 if __name__ == "__main__":
